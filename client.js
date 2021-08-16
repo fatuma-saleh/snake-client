4b48743e8166788aeae87e187e94e0c00b61c49d
Fatuma <sfatex@gmail.com>
2021-08-16 16:16:58 -0400

    Added snake initials in the client.js

--- client.js
+++ client.js
@@ -1,15 +1,21 @@
 const net = require('net');
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: "localhost",
     port: 50541
   });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
 
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on("connect", () => {//when connection is established
+    console.log("Successfully connected!!")
+    conn.write("Name: FSS")
+    conn.write("Move: up")
+  })
   conn.on('data', (data) => {
     console.log('message from server: ', data);
-  
+
   });
   return conn;
 }
