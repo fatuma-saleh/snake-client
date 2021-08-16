abae2044edaa3600754a766b2b991787e08b6df9
Fatuma <sfatex@gmail.com>
2021-08-16 17:18:34 -0400

    added movements to the snake

--- client.js
+++ client.js
@@ -5,18 +5,24 @@ const connect = function () {
     port: 50541
   });
 
-
   // interpret incoming data as text
   conn.setEncoding('utf8');
+
   conn.on("connect", () => {//when connection is established
     console.log("Successfully connected!!")
-    conn.write("Name: FSS")
-    conn.write("Move: up")
+    conn.write("Name: FSS")//writing  my initials to the object in the server
+    //conn.write("Move: up")
+    //conn.write("Move: down")
   })
+  // setTimeout(() =>(conn.write("Move: up"),1000))
+  // setTimeout(() =>(conn.write("Move: up"),4000))
+  //setInterval(() => conn.write("Move: up"), 50)
   conn.on('data', (data) => {
     console.log('message from server: ', data);
 
+
   });
+
   return conn;
 }
 
