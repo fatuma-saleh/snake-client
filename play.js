6bbbb906da8655e545448ea6ffde5ef12634bdb4
Fatuma <sfatex@gmail.com>
2021-08-16 19:09:40 -0400

    created input.js to handle functions related to user input

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
-const net = require('net');
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
+
 connect();//function call
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  handleUserInput();
-  return stdin;
-}
-
-const handleUserInput = function () {
-  const stdin = process.stdin;
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-}
 setupInput();
