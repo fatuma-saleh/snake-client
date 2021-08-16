7bc631b0c60ecbf18cc65d68a8a261a7b07404b8
Fatuma <sfatex@gmail.com>
2021-08-16 18:55:39 -0400

    added stdin setup functions

--- play.js
+++ play.js
@@ -1,5 +1,23 @@
 const net = require('net');
 const { connect } = require('./client');
-
 console.log('Connecting ...');
 connect();//function call
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+}
+
+const handleUserInput = function () {
+  const stdin = process.stdin;
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+}
+setupInput();
